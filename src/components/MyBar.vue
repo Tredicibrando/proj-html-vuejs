7726dbfa792f2be6ecc3eeb6c187ca52dbfceb08
Tredicibrando <nicolosigiov@gmail.com>
2022-03-21 16:42:57 +0100

    Struttura_Header_2

---
+++ src/components/MyBar.vue
@@ -0,0 +1,96 @@
+<template>
+    <div class="fixed_bar">
+        <div class="container">
+            <div class="row">
+                <div class="hour">
+                    <i class="fa-solid fa-clock"></i>
+                    <p> Open Hours: Mon - Sat 9:00 - 18:00</p>
+                </div>
+
+                <div class="info">
+                    <i class="fa-solid fa-phone"></i>
+                    <p>+1 (305) 1234 5678</p>
+                    <i class="fa-solid fa-envelope"></i>
+                    <p>hello@example.com</p>
+
+                    <div class="social">
+                        <i class="fa-brands fa-facebook-f"></i>
+                        <i class="fa-brands fa-twitter"></i>
+                        <i class="fa-brands fa-linkedin-in"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'MyBar'
+    }
+</script>
+
+<style lang="scss" scoped>
+
+$limed: #3d4955;
+
+.fixed_bar{
+    position: fixed;
+    background-color: $limed;
+    width: 100%;
+    color: white;
+
+    .container{
+        width: 1240px;
+        margin: 0 auto;
+
+        .row{
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+            padding: 7px 0;
+            font-weight: 200;
+
+            i{
+                font-size: 14px;
+                opacity: 70%;
+
+                &:hover{
+                    opacity: 100%;
+                }
+            }
+
+            
+
+            .hour{
+                display: flex;
+                align-items: center;
+                gap: 5px;
+
+
+
+            }
+
+            .info{
+                display: flex;
+                align-items: center;
+                gap: 10px;
+
+                .fa-phone, 
+                .fa-envelope{
+                    margin-right: 10px;
+                }
+
+                .social{
+                    display: flex;
+                    align-items: center;
+                    gap: 15px;
+                    margin-left: 10px;
+                    
+                }
+            }
+        }
+    }
+}
+
+</style>
---
+++ src/components/MyJumbo.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="jumbo">
+        <MyNav />
+
+
+    </div>
+</template>
+
+<script>
+
+import MyNav from '@/components/MyNav.vue'
+
+    export default {
+        name: 'MyJumbo',
+
+        components:{
+            MyNav
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    .jumbo{
+        padding-top: 25px;
+    }
+</style>
