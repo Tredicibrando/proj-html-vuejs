af07dbf8d20cbccfacdf6dddee00ac9510efce91
Tredicibrando <nicolosigiov@gmail.com>
2022-03-21 16:42:44 +0100

    Struttura_Header

---
+++ src/components/MyHeader.vue
@@ -0,0 +1,31 @@
+<template>
+    <header class="header">
+        <MyBar />
+        
+    </header>
+</template>
+
+<script>
+
+import MyBar from './MyBar.vue'
+
+    export default {
+        name: 'MyHeader',
+
+        components: {
+        MyBar,
+        }   
+    }
+
+    
+</script>
+
+<style lang="scss" scoped>
+.header{
+    width: 100%;
+    background-image: url(../assets/img/bg-6.jpg);
+    min-height: calc(100vh - 40px);
+    background-repeat: no-repeat;
+    background-size: 100%;
+}
+</style>
---
+++ src/components/MyNav.vue
@@ -0,0 +1,15 @@
+<template>
+    <ul>
+        <li> <img src="" alt=""></li>
+    </ul>
+</template>
+
+<script>
+    export default {
+        name: 'MyNav'
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
