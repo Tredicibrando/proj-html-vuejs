2545f2330a0d170ec25e381bef5a06b1dad9d203
Tredicibrando <nicolosigiov@gmail.com>
2022-03-27 14:03:46 +0200

    footer_2

---
+++ src/components/MyFooter.vue
@@ -0,0 +1,123 @@
+<template>
+    <footer class="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-25">
+                    <figure class="logo_wrapper">
+                        <img src="../assets/img/nexgen-logo.svg" alt="">
+                    </figure>
+                    <p class="paragraph">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, consequatur?
+                    </p>
+                    <ul class="list_wrapper">
+                        <li class="list_item">
+                            <i class="fa-solid fa-phone"></i>
+                            <a href="#">+1 (305) 1234-56789</a>
+                        </li>
+                        <li class="list_item">
+                            <i class="fa-solid fa-envelope"></i>
+                            <a href="#">hello@example.com</a>
+                        </li>
+                        <li class="list_item">
+                            <i class="fa-solid fa-location-dot"></i>
+                            <a href="#">Main Avenue, 987</a>
+                        </li>
+                    </ul>
+                    <input class="footer-btn" type="button" value="Get in Touch">
+                </div>
+                <div>
+                    <div class="col-25">
+                        <h3>About</h3>
+                        <ul>
+                            <li>list item</li>
+                        </ul>
+                    </div>
+                    <div class="col-25"></div>
+                    <div class="col-25"></div>
+                </div>
+            </div>
+        </div>
+
+        <FooterBar />
+    </footer>
+</template>
+
+<script>
+
+import FooterBar from '@/components/FooterBar.vue'
+
+    export default {
+        name: 'MyFooter',
+        components:{
+            FooterBar,
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+.footer{
+    background-color: #111117;
+    padding: 100px 0;
+    color: white;
+
+    .container{
+        width: 1240px;
+        margin: 0 auto;
+
+        .row{
+            display: flex;
+            gap: 25px;
+
+            .col-25{
+                width: 25%;
+
+                .logo_wrapper{
+                width: max-content;
+                margin-bottom: 25px;
+
+                    img{
+                        width: 100px;
+                
+                    }
+
+                }
+
+               .paragraph{
+                   margin-bottom: 25px;
+                   color: #ddd;
+                   font-size: 14px;
+               }
+
+                .list_wrapper{
+                    display: flex;
+                    flex-direction: column;
+                    gap: 10px;
+                    color: #ddd;
+                    
+
+                    .list_item{
+                        display: flex;
+                        gap: 10px;
+                        align-items: center;
+                    }
+                }
+
+                .footer-btn{
+                    margin-top: 25px;
+                    padding: 10px 15px;
+                    border: 1px solid #058283;
+                    color: white;
+                    text-transform: uppercase;
+                    background-color: transparent;
+                    border-radius: 3px;
+                }
+            }
+
+           
+
+
+
+        }
+    }
+}
+</style>
--- src/components/MyNewsletter.vue
+++ src/components/MyNewsletter.vue
@@ -3,12 +3,16 @@
         <div class="container">
             <div class="row">
                 <div class="col-50">
-                    <h4>Newsletter</h4>
-                    <h3><span>Know</span> First</h3>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, vitae!</p>
+                    <h4 class="subtitle">Newsletter</h4>
+                    <h3 class="title"><span class="rect_decor">Know</span> First</h3>
+                    <p class="paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, vitae!</p>
                 </div>
                 <div class="col-50">
-
+                    <div class="input_wrapper">
+                        <input class="input_text" type="text" placeholder="Name">
+                        <input class="input_text" type="text" placeholder="Email">
+                        <input class="input_button" type="button" value="subscribe">
+                    </div>
                 </div>
             </div>
         </div>
@@ -24,10 +28,11 @@
 
 <style lang="scss" scoped>
 .newsletter{
-    padding: 100px 0; 
+    padding: 150px 0; 
     background-image: url(../assets/img/bg-1.jpg);
     background-size: cover;
     background-position: center;
+    color: white;
 
     .container{
         width: 1240px;
@@ -35,10 +40,64 @@
 
         .row{
             display: flex;
-            padding: 0 25px;
+            padding: 0 35px;
 
             .col-50{
                 width: 50%;
+
+                .subtitle{
+                    color: #45bbbc;
+                    text-transform: uppercase;
+                    margin-bottom: 25px;
+                }
+
+                .title{
+                    font-size: 32px;
+                    font-weight: 800;
+                     margin-bottom: 25px;
+                    
+                    .rect_decor{
+                    background-color: rgba($color: #45bbbc, $alpha: 0.2) ;
+                    padding: 5px 10px;
+                    border-radius: 3px;
+                    color: white;
+                    }
+                }
+
+                .paragraph{
+                    color: #ddd;
+                    font-size: 14px;
+                }
+
+                .input_wrapper{
+                    display: flex;
+                    flex-direction: column;
+                    gap: 15px;
+
+                    .input_text{
+                        width: 400px;
+                        padding: 10px 25px;
+                        opacity: 0.5;
+                        border-radius: 3px;
+                        border: none;
+                    }
+
+                    .input_button{
+                        padding: 10px 15px;
+                        background-color: #22918e;
+                        border: none;
+                        text-transform: uppercase;
+                        color: white;
+                        border-radius: 3px;
+                        width: 120px;
+                        box-shadow: 0 0 15px rgb(19 19 19 / 10%);
+                    }
+
+                }
+
+
+
+                
             }
         }
     }
